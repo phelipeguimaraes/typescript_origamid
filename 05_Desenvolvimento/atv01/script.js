2591bfe21d26da209c22ef09296e1562c75bc74d
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-05-01 15:44:29 -0300

    TSConfig conhecendo as configurações do TS

--- 05_Desenvolvimento/atv01/script.js
+++ 05_Desenvolvimento/atv01/script.js
@@ -1,4 +1,27 @@
-import plugin from "./plugin.js";
-plugin();
+"use strict";
+function handleProduto(dados) {
+    dados;
+}
+let link = document.querySelector("a");
+if (link) {
+    link.innerHTML = "teste";
+}
 const teste = "teste";
+function handleClick() {
+    console.log(this);
+}
+document.documentElement.addEventListener("click", handleClick);
+function trocarModo(modo) {
+    if (modo === "dark") {
+        return "color: black";
+    }
+    else {
+        return "color: white";
+    }
+}
+trocarModo("light");
+function maiuscula(frase) {
+    console.log(frase);
+    const total = 100;
+}
 //# sourceMappingURL=script.js.map
--- 05_Desenvolvimento/atv01/script.ts
+++ 05_Desenvolvimento/atv01/script.ts
@@ -1,7 +1,41 @@
-import plugin from "./plugin.js"
+// import plugin from "./plugin.js"
 
-plugin()
+// plugin()
 
-// document.querySelector("a").innerHTML = "teste"
+interface Produto {
+   nome: string;
+   preco: number
+}
+
+function handleProduto(dados: any) {
+   dados
+}
+
+let link = document.querySelector("a")
+if(link) {
+   link.innerHTML = "teste"
+}
 
 const teste = "teste"
+
+function handleClick(this: HTMLElement) {
+   console.log(this)
+}
+
+document.documentElement.addEventListener("click", handleClick)
+
+function trocarModo(modo: string) {
+   if(modo === "dark") {
+      return "color: black"
+   } else {
+      return "color: white"
+   }
+}
+
+trocarModo("light")
+
+function maiuscula(frase: string) {
+   console.log(frase)
+   const total = 100
+}
+
