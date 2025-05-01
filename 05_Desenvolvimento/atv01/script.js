4affe907dd41f27a8dab3af604092c58dc087b54
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2025-05-01 12:44:12 -0300

    Desenvolvimento TSConfig

---
+++ 05_Desenvolvimento/atv01/script.js
@@ -0,0 +1,4 @@
+import plugin from "./plugin.js";
+plugin();
+const teste = "teste";
+//# sourceMappingURL=script.js.map
---
+++ 05_Desenvolvimento/atv01/script.ts
@@ -0,0 +1,7 @@
+import plugin from "./plugin.js"
+
+plugin()
+
+// document.querySelector("a").innerHTML = "teste"
+
+const teste = "teste"
